f1e862d9bc614448c0d02a845079b6780b2b3fc0
DmitryS <diamond005@mail.ru>
2022-12-17 17:17:26 +0300

    Решение второй задачи готово

--- Desktop/Обучение GB/Семинар 7/DZ_S7/Program.cs
+++ Desktop/Обучение GB/Семинар 7/DZ_S7/Program.cs
@@ -28,4 +28,33 @@
 // }
 
 // ___**Задача 2**___
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введи первое число");
+// int a=Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine("Введи второе число");
+// int b=Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine("Введи третье число");
+// int c=Convert.ToInt32 (Console.ReadLine());
+// int max=a;
+// if (a>b)
+// {
+//     if (a>c)
+//     {
+//         max=a;
+//     }
+//     else max=c; 
+// }
+// else if (b>c)
+// {
+//     max=b;  
+// }
+// else max=c;
+// Console.WriteLine("Самое большое:");
+// Console.WriteLine(max);
+
+//____**Задача 3**_____
 
