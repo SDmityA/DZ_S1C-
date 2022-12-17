207ca13e9090c0b5cfc4477b0d96c73072ad5cea
DmitryS <diamond005@mail.ru>
2022-12-17 16:57:40 +0300

    Решение первой задачи готово

--- Desktop/Обучение GB/Семинар 7/DZ_S7/Program.cs
+++ Desktop/Обучение GB/Семинар 7/DZ_S7/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Введи число");
+﻿// ___**Задача 1**_____
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Введи первое число");
+// int num=Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine("Введи второе число");
+// int num1=Convert.ToInt32 (Console.ReadLine());
+// if (num>num1)
+// {
+//     Console.WriteLine("Большее: "); 
+//     Console.WriteLine(num);
+//     Console.WriteLine("Меньшее: "); 
+//     Console.WriteLine(num1);
+// }
+// else if (num1>num)
+// {
+//    Console.WriteLine("Большее: "); 
+//    Console.WriteLine(num1);
+//    Console.WriteLine("Меньшее: "); 
+//    Console.WriteLine(num);
+// }
+// else if (num==num1)
+// {
+//     Console.WriteLine("Они равны");
+// }
+
+// ___**Задача 2**___
+
