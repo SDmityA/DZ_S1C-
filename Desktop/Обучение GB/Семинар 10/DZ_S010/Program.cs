b2a827e9b56caebed8a17734404aa57859482e32
DmitryS <diamond005@mail.ru>
2022-12-22 22:32:09 +0300

    первая задача решена

--- Desktop/Обучение GB/Семинар 10/DZ_S010/Program.cs
+++ Desktop/Обучение GB/Семинар 10/DZ_S010/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// ****************Задача 1************************
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// Console.WriteLine("Введите первое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int res=1;
+// for (int i = 1; i <= num1; i++)
+// {
+//     res=res*num;
+// }
+// Console.WriteLine(res);
+
+// ********************************конец*****************************
+// ****************************Задача 2******************************
+
