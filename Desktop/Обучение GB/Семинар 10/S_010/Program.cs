91e5e7915a58af7d5efe3d4524b8df44c3c7b228
DmitryS <diamond005@mail.ru>
2022-12-22 19:57:30 +0300

    Начали семинар

---
+++ Desktop/Обучение GB/Семинар 10/S_010/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// *****************Задача 1**********************************
+// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+Console.WriteLine("Введите число");
+int A=Convert.ToInt32(Console.ReadLine());
