1f2f74879dc8daabcdcf21dc4f802492ef328d8a
DmitryS <diamond005@mail.ru>
2022-12-22 20:35:53 +0300

    Решили вторую задау

--- Desktop/Обучение GB/Семинар 10/S_010/Program.cs
+++ Desktop/Обучение GB/Семинар 10/S_010/Program.cs
@@ -6,5 +6,48 @@ Console.WriteLine("Hello, World!");
 // 7 -> 28
 // 4 -> 10
 // 8 -> 36
-Console.WriteLine("Введите число");
-int A=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int A=Convert.ToInt32(Console.ReadLine());
+
+// int getsum()
+// {
+//     int sum=0;
+//     for (int i = 0; i <= A; i++)
+//     {
+//         sum=sum+i;
+//     }
+//     return sum;
+// }
+// Console.WriteLine(getsum());
+
+// *******************конец**********************
+// ************Задача2************************
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Console.WriteLine("Введите число");
+// int num=Convert.ToInt32(Console.ReadLine());
+// string num1=Convert.ToString(num);
+// int size=num1.Length;
+// Console.WriteLine(size);
+
+// Console.WriteLine("Введите число");
+// int num=Convert.ToInt32(Console.ReadLine());
+
+// int GetCount()
+// {
+//     int count = 0;
+//     while (num>0)
+//     {
+//         count++;
+//         num=num/10;
+//     }
+    
+//     return count;
+// }
+// Console.WriteLine(GetCount());
+
+// ****************конец**************
+// ***************Задача 3*************
